a99d740d63fe0c99546d3cd1d55335f76bb8fed7
Tomás <tomasarreguib@gmail.com>
2024-03-20 22:30:02 -0300

    updated ex1 file. Finished first exercise

--- tp1/ex1.go
+++ tp1/ex1.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+/* 
+  Dado el siguiente algoritmo:
 
-func main() {
-  fmt.Println("hello world")
-}
+*/
+
+  func buscar(lista []string, buscado string) int {
+    for i := 0; i < len(lista); i++ {
+      if lista[i] == buscado {
+        return i
+      }
+    }
+
+    return -1
+  }
+
+/*
+  a) Calcule T(n) en el peor caso. ¿Cuál es el Orden del algoritmo? O(n)
+
+  b) ¿Podemos decir también que el algoritmo tiene O(n²)? ¿Por qué? 
+     No. Porque la complejidad aumenta a medida que se añaden elementos a la lista y en ningún momento se la va a recorrer dos veces
+
+  c) ¿Podemos decir también que el algoritmo tiene O(log² n)? ¿Por qué?
+     No. Para ser un log² n, la búsqueda debería dividirse a la mitad en cada iteración, así como lo hace Binary Search, donde en cada iteración se divide la cantidad de elementos a la mitad y se descartar una de ellas en base al número que se está buscando. 
+
+  d) ¿Podemos decir también que el algoritmo tiene O(1)? ¿Por qué?
+     No. Porque para ser constante debería ejecutarse un sólo paso sin importar la cantidad de elementos en la lista.
+*/
