0ce9b5481e2009455df1aa4dfd7da80a56dba4a1
Tomás <tomasarreguib@gmail.com>
2024-03-21 22:29:23 -0300

    ex4 resolution

---
+++ tp1/ex4.go
@@ -0,0 +1,11 @@
+package main
+
+/*
+
+  Si en el ejercicio 2 el tamaño de la entrada es 3. ¿Cual utilizaría? (Se refiere a elegir entre O(n²) y O(2^n))
+
+  Sí y solo sí, preferiría usar O(2^n), siempre y cuando tenga algo que me asegure que la entrada
+  fuera constantemente 3 y no aumentara a lo largo del tiempo bajo ningún concepto, ya que
+  al dispararse el número de entradas, lo haría también la complejidad del algoritmo.
+*/
+
