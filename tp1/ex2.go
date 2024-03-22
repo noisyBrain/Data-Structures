65c039f8b534ca66f44d0711f8f94150a6760476
Tomás <tomasarreguib@gmail.com>
2024-03-21 21:53:12 -0300

    ex2 resolution

---
+++ tp1/ex2.go
@@ -0,0 +1,12 @@
+package main
+
+/* 
+  Si se tiene un algoritmo con complejidad computacional O(n²) y otro con O (2^n),
+  donde n es el tamaño de la entrada. ¿Cuál utilizaría usted?
+
+  Eligiría usar el algoritmo de complejidad computacional O(n²) ya que, a pesar de ser cuadrática y no
+  el más eficiente, la complejidad de este no se vería afectada ante la variación de las entradas.
+  Por el contrario, en el algoritmo de complejidad O(2^n), crecería su exponenete a medida que crece
+  la entrada
+  
+*/
