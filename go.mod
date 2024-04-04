32d9b483b2fb3fab892c939736e85643b0ae3a7d
Tomás <tomasarreguib@gmail.com>
2024-04-04 16:15:47 -0300

    added go mod and main for modules purposes

---
+++ go.mod
@@ -0,0 +1,3 @@
+module tps
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "fmt"
+  "tps/tp2"
+)
+
+func main() {
+  // ex 2
+  fmt.Println(tp2.SumInts(1, 3))
+
+  // ex 3
+  fmt.Println(tp2.Factorial(5))
+}
