28b61d85a28eb4ddd7c5c1f93bb1f0f358316c9b
Tomás <tomasarreguib@gmail.com>
2024-04-04 16:58:21 -0300

    ex 4 resolution added to the exercise some extra validations for special characters

--- main.go
+++ main.go
@@ -11,4 +11,7 @@ func main() {
 
   // ex 3
   fmt.Println(tp2.Factorial(5))
+
+  // ex 4
+  fmt.Println(tp2.VowelsAndConsonants("#testeando <la> @ funcion!!")) // should return 8 vowels, 10 consonants
 }
---
+++ tp2/ex4.go
@@ -0,0 +1,29 @@
+package tp2
+
+import (
+	"strings"
+)
+
+func VowelsAndConsonants(str string) (int, int) {
+  if len(str) == 0 {
+    return 0, 0
+  }
+
+  totalVowels, totalConsonants := VowelsAndConsonants(str[1:])
+  vowels := "aeiou"
+  specialChars := "!@#$%^&*()<>¿?"
+
+  currentChar := string(str[0])
+  isSpecialChar := currentChar == " " || strings.Contains(specialChars, currentChar)
+  isVowel := strings.Contains(vowels, currentChar)
+
+  if isSpecialChar {
+    return totalVowels, totalConsonants
+  }
+
+  if isVowel {
+    return totalVowels + 1, totalConsonants
+  }
+
+  return totalVowels, totalConsonants + 1
+}
