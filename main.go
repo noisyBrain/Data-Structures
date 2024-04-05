569e7b83b7c6e44f7dadf35735f3eb7f1ad56500
Tomás <tomasarreguib@gmail.com>
2024-04-04 21:41:26 -0300

    updated main added a print for each exercise to be more specific in the output

--- main.go
+++ main.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
   // ex 2
+  fmt.Println("\nExercise 2:")
   fmt.Println(tp2.SumInts(1, 3))
 
   // ex 3
+  fmt.Println("\nExercise 3:")
   fmt.Println(tp2.Factorial(5))
 
   // ex 4
+  fmt.Println("\nExercise 4:")
   fmt.Println(tp2.VowelsAndConsonants("#testeando <la> @ funcion!!")) // should return 8 vowels, 10 consonants
+
+  // ex 5
+  fmt.Println("\nExercise 5:")
+  fmt.Println(tp2.FirstMayusPosition("hola como Te va")) // should return 10
 }
