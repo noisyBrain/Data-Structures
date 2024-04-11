ec41284f3c61802137900dfd0fb01a27e1bb1f6b
Tomás <tomasarreguib@gmail.com>
2024-04-11 19:35:33 -0300

    added resolution for exercise 7

---
+++ tp2/ex7.go
@@ -0,0 +1,17 @@
+/*
+
+  Invertir: dado un arreglo de enteros, invertirlo.
+
+*/
+
+package tp2
+
+func invertir(arr []int, aux []int) []int {
+  if len(arr) == 0 {
+    return aux
+  }
+
+  aux = append(aux, arr[len(arr) - 1])
+  
+  return invertir(arr[:len(arr) - 1], aux);
+}
