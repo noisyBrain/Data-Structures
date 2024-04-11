abf3b5b0176b03c2256da2faf00ae90029d22d15
Tomás <tomasarreguib@gmail.com>
2024-04-11 19:33:57 -0300

    added resolution for exercise 6

---
+++ tp2/ex6.go
@@ -0,0 +1,19 @@
+/*
+
+  Mayor elemento: dado un arreglo de enteros, devolver el mayor elemento
+
+*/
+
+package tp2
+
+func greaterElement(arr []int, gt int) int {
+  if len(arr) == 0 {
+    return gt
+  }
+
+  if gt < arr[0] {
+    gt = arr[0]
+  }
+
+  return greaterElement(arr[1:], gt)
+}
